cc0f8093b8819c77159f3d9db25c0a42a1537648
Nathan.Edwards@asu.edu <Nathan.Edwards@asu.edu@d400dafa-9ea4-11de-a8ff-c955b97a4a10>
2009-10-30 08:13:07 +0000

    Fixed DLL error handling and messaging.  Still more work to be done, but is functional

--- wireless_haptic_belt/code/haptikosPDA/HaptikosPC/HaptikosPC/MainForm.designer.cs
+++ wireless_haptic_belt/code/haptikosPDA/HaptikosPC/HaptikosPC/MainForm.designer.cs
@@ -26,13 +26,15 @@ namespace Haptikos
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
             this.mnuClose = new System.Windows.Forms.MenuItem();
             this.menuItem2 = new System.Windows.Forms.MenuItem();
             this.mnuSettings = new System.Windows.Forms.MenuItem();
             this.mnuConnect = new System.Windows.Forms.MenuItem();
             this.mnuDisconnect = new System.Windows.Forms.MenuItem();
             this.mnuDemo = new System.Windows.Forms.MenuItem();
+            this.mnuTempSpat = new System.Windows.Forms.MenuItem();
             this.txtMess = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.txtLog = new System.Windows.Forms.TextBox();
@@ -51,52 +53,66 @@ namespace Haptikos
             this.btnQuery = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.btnActivate = new System.Windows.Forms.Button();
-            this.mnuTempSpat = new System.Windows.Forms.MenuItem();
             this.tabControl1.SuspendLayout();
             this.tabPageMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainMenu1
             // 
-            this.mainMenu1.MenuItems.Add(this.mnuClose);
-            this.mainMenu1.MenuItems.Add(this.menuItem2);
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.mnuClose,
+            this.menuItem2});
             // 
             // mnuClose
             // 
+            this.mnuClose.Index = 0;
             this.mnuClose.Text = "Close";
             this.mnuClose.Click += new System.EventHandler(this.mnuClose_Click);
             // 
             // menuItem2
             // 
-            this.menuItem2.MenuItems.Add(this.mnuSettings);
-            this.menuItem2.MenuItems.Add(this.mnuConnect);
-            this.menuItem2.MenuItems.Add(this.mnuDisconnect);
-            this.menuItem2.MenuItems.Add(this.mnuDemo);
-            this.menuItem2.MenuItems.Add(this.mnuTempSpat);
+            this.menuItem2.Index = 1;
+            this.menuItem2.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.mnuSettings,
+            this.mnuConnect,
+            this.mnuDisconnect,
+            this.mnuDemo,
+            this.mnuTempSpat});
             this.menuItem2.Text = "Menu";
             // 
             // mnuSettings
             // 
+            this.mnuSettings.Index = 0;
             this.mnuSettings.Text = "Settings";
             this.mnuSettings.Click += new System.EventHandler(this.mnuSettings_Click);
             // 
             // mnuConnect
             // 
             this.mnuConnect.Enabled = false;
+            this.mnuConnect.Index = 1;
             this.mnuConnect.Text = "Connect";
             this.mnuConnect.Click += new System.EventHandler(this.mnuConnect_Click);
             // 
             // mnuDisconnect
             // 
             this.mnuDisconnect.Enabled = false;
+            this.mnuDisconnect.Index = 2;
             this.mnuDisconnect.Text = "Disconnect";
             this.mnuDisconnect.Click += new System.EventHandler(this.mnuDisconnect_Click);
             // 
             // mnuDemo
             // 
             this.mnuDemo.Enabled = false;
+            this.mnuDemo.Index = 3;
             this.mnuDemo.Text = "Demo";
             this.mnuDemo.Click += new System.EventHandler(this.mnuDemo_Click);
+            // 
+            // mnuTempSpat
+            // 
+            this.mnuTempSpat.Enabled = false;
+            this.mnuTempSpat.Index = 4;
+            this.mnuTempSpat.Text = "Temp-Spatial";
+            this.mnuTempSpat.Click += new System.EventHandler(this.mnuTempSpat_Click);
             // 
             // txtMess
             // 
@@ -125,21 +141,23 @@ namespace Haptikos
             this.txtLog.Name = "txtLog";
             this.txtLog.ReadOnly = true;
             this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtLog.Size = new System.Drawing.Size(234, 91);
+            this.txtLog.Size = new System.Drawing.Size(234, 79);
             this.txtLog.TabIndex = 3;
             // 
             // labelStatus
             // 
-            this.labelStatus.Location = new System.Drawing.Point(3, 214);
+            this.labelStatus.Location = new System.Drawing.Point(2, 201);
             this.labelStatus.Name = "labelStatus";
             this.labelStatus.Size = new System.Drawing.Size(45, 20);
+            this.labelStatus.TabIndex = 22;
             this.labelStatus.Text = "Status:";
             // 
             // labelStatusMsg
             // 
-            this.labelStatusMsg.Location = new System.Drawing.Point(47, 214);
+            this.labelStatusMsg.Location = new System.Drawing.Point(46, 201);
             this.labelStatusMsg.Name = "labelStatusMsg";
-            this.labelStatusMsg.Size = new System.Drawing.Size(190, 31);
+            this.labelStatusMsg.Size = new System.Drawing.Size(190, 41);
+            this.labelStatusMsg.TabIndex = 21;
             this.labelStatusMsg.Text = "status label";
             // 
             // tabControl1
@@ -169,75 +187,80 @@ namespace Haptikos
             this.tabPageMain.Controls.Add(this.txtLog);
             this.tabPageMain.Controls.Add(this.txtMess);
             this.tabPageMain.Controls.Add(this.labelStatus);
-            this.tabPageMain.Location = new System.Drawing.Point(0, 0);
+            this.tabPageMain.Location = new System.Drawing.Point(4, 22);
             this.tabPageMain.Name = "tabPageMain";
-            this.tabPageMain.Size = new System.Drawing.Size(240, 245);
+            this.tabPageMain.Size = new System.Drawing.Size(232, 242);
+            this.tabPageMain.TabIndex = 0;
             this.tabPageMain.Text = "Haptics Main";
             // 
             // comboBoxCycles
             // 
-            this.comboBoxCycles.Location = new System.Drawing.Point(183, 145);
+            this.comboBoxCycles.Location = new System.Drawing.Point(182, 132);
             this.comboBoxCycles.Name = "comboBoxCycles";
-            this.comboBoxCycles.Size = new System.Drawing.Size(44, 22);
+            this.comboBoxCycles.Size = new System.Drawing.Size(44, 21);
             this.comboBoxCycles.TabIndex = 16;
             // 
             // labelCyc
             // 
-            this.labelCyc.Location = new System.Drawing.Point(183, 129);
+            this.labelCyc.Location = new System.Drawing.Point(182, 116);
             this.labelCyc.Name = "labelCyc";
             this.labelCyc.Size = new System.Drawing.Size(44, 20);
+            this.labelCyc.TabIndex = 17;
             this.labelCyc.Text = "Cycles";
             this.labelCyc.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // comboBoxMag
             // 
-            this.comboBoxMag.Location = new System.Drawing.Point(121, 145);
+            this.comboBoxMag.Location = new System.Drawing.Point(120, 132);
             this.comboBoxMag.Name = "comboBoxMag";
-            this.comboBoxMag.Size = new System.Drawing.Size(56, 22);
+            this.comboBoxMag.Size = new System.Drawing.Size(56, 21);
             this.comboBoxMag.TabIndex = 13;
             // 
             // labelMag
             // 
-            this.labelMag.Location = new System.Drawing.Point(115, 129);
+            this.labelMag.Location = new System.Drawing.Point(114, 116);
             this.labelMag.Name = "labelMag";
             this.labelMag.Size = new System.Drawing.Size(68, 20);
+            this.labelMag.TabIndex = 18;
             this.labelMag.Text = "Magnitude";
             this.labelMag.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // comboBoxRhy
             // 
-            this.comboBoxRhy.Location = new System.Drawing.Point(62, 145);
+            this.comboBoxRhy.Location = new System.Drawing.Point(61, 132);
             this.comboBoxRhy.Name = "comboBoxRhy";
-            this.comboBoxRhy.Size = new System.Drawing.Size(53, 22);
+            this.comboBoxRhy.Size = new System.Drawing.Size(53, 21);
             this.comboBoxRhy.TabIndex = 10;
             // 
             // labelRhy
             // 
-            this.labelRhy.Location = new System.Drawing.Point(62, 129);
+            this.labelRhy.Location = new System.Drawing.Point(61, 116);
             this.labelRhy.Name = "labelRhy";
             this.labelRhy.Size = new System.Drawing.Size(53, 20);
+            this.labelRhy.TabIndex = 19;
             this.labelRhy.Text = "Rhythm";
             this.labelRhy.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // comboBoxMotor
             // 
-            this.comboBoxMotor.Location = new System.Drawing.Point(12, 145);
+            this.comboBoxMotor.Location = new System.Drawing.Point(11, 132);
             this.comboBoxMotor.Name = "comboBoxMotor";
-            this.comboBoxMotor.Size = new System.Drawing.Size(44, 22);
+            this.comboBoxMotor.Size = new System.Drawing.Size(44, 21);
             this.comboBoxMotor.TabIndex = 7;
             // 
             // labelMtr
             // 
-            this.labelMtr.Location = new System.Drawing.Point(12, 129);
+            this.labelMtr.Location = new System.Drawing.Point(11, 116);
             this.labelMtr.Name = "labelMtr";
             this.labelMtr.Size = new System.Drawing.Size(44, 20);
+            this.labelMtr.TabIndex = 20;
             this.labelMtr.Text = "Motor";
             this.labelMtr.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // btnQuery
             // 
             this.btnQuery.Enabled = false;
-            this.btnQuery.Location = new System.Drawing.Point(138, 173);
+            this.btnQuery.Location = new System.Drawing.Point(137, 160);
             this.btnQuery.Name = "btnQuery";
             this.btnQuery.Size = new System.Drawing.Size(99, 38);
             this.btnQuery.TabIndex = 6;
@@ -247,7 +270,7 @@ namespace Haptikos
             // btnStop
             // 
             this.btnStop.Enabled = false;
-            this.btnStop.Location = new System.Drawing.Point(78, 173);
+            this.btnStop.Location = new System.Drawing.Point(77, 160);
             this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(54, 38);
             this.btnStop.TabIndex = 5;
@@ -257,48 +280,27 @@ namespace Haptikos
             // btnActivate
             // 
             this.btnActivate.Enabled = false;
-            this.btnActivate.Location = new System.Drawing.Point(3, 173);
+            this.btnActivate.Location = new System.Drawing.Point(2, 160);
             this.btnActivate.Name = "btnActivate";
             this.btnActivate.Size = new System.Drawing.Size(69, 38);
             this.btnActivate.TabIndex = 4;
             this.btnActivate.Text = "Activate";
             this.btnActivate.Click += new System.EventHandler(this.btnActivate_Click);
             // 
-            // mnuTempSpat
-            // 
-            this.mnuTempSpat.Enabled = false;
-            this.mnuTempSpat.Text = "Temp-Spatial";
-            this.mnuTempSpat.Click += new System.EventHandler(this.mnuTempSpat_Click);
-            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(240, 285);
+            this.ClientSize = new System.Drawing.Size(257, 302);
             this.Controls.Add(this.tabControl1);
-            //this.Controls.Add(this.comboBoxCycles);
-            //this.Controls.Add(this.labelCyc);
-            //this.Controls.Add(this.comboBoxMag);
-            //this.Controls.Add(this.labelMag);
-            //this.Controls.Add(this.comboBoxRhy);
-            //this.Controls.Add(this.labelRhy);
-            //this.Controls.Add(this.comboBoxMotor);
-            //this.Controls.Add(this.labelMtr);
-            //this.Controls.Add(this.btnQuery);
-            //this.Controls.Add(this.btnStop);
-            //this.Controls.Add(this.btnActivate);
-            //this.Controls.Add(this.labelStatusMsg);
-            //this.Controls.Add(this.btnSend);
-            //this.Controls.Add(this.txtLog);
-            //this.Controls.Add(this.txtMess);
-            //this.Controls.Add(this.labelStatus);
             this.Menu = this.mainMenu1;
             this.Name = "MainForm";
             this.Text = "Haptikos";
             this.Closing += new System.ComponentModel.CancelEventHandler(this.Form1_Closing);
             this.tabControl1.ResumeLayout(false);
             this.tabPageMain.ResumeLayout(false);
+            this.tabPageMain.PerformLayout();
             this.ResumeLayout(false);
 
         }
--- wireless_haptic_belt/code/haptikosPDA/HaptikosPC/HaptikosPC/SettingsForm.Designer.cs
+++ wireless_haptic_belt/code/haptikosPDA/HaptikosPC/HaptikosPC/SettingsForm.Designer.cs
@@ -29,13 +29,15 @@ namespace Haptikos
         /// </summary>
         private void InitializeComponent()
         {
-            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.comboBoxInbound = new System.Windows.Forms.ComboBox();
             this.comboBoxOutbound = new System.Windows.Forms.ComboBox();
+            this.checkBoxComPortSame = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
@@ -43,6 +45,7 @@ namespace Haptikos
             this.label1.Location = new System.Drawing.Point(12, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 13;
             this.label1.Text = "Inbound";
             // 
             // label2
@@ -50,6 +53,7 @@ namespace Haptikos
             this.label2.Location = new System.Drawing.Point(12, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 12;
             this.label2.Text = "Outbound";
             // 
             // btnOK
@@ -74,15 +78,31 @@ namespace Haptikos
             // 
             this.comboBoxInbound.Location = new System.Drawing.Point(81, 30);
             this.comboBoxInbound.Name = "comboBoxInbound";
-            this.comboBoxInbound.Size = new System.Drawing.Size(100, 22);
+            this.comboBoxInbound.Size = new System.Drawing.Size(100, 21);
             this.comboBoxInbound.TabIndex = 10;
+            this.comboBoxInbound.SelectedIndexChanged += new System.EventHandler(this.comboBoxInbound_SelectedIndexChanged);
             // 
             // comboBoxOutbound
             // 
+            this.comboBoxOutbound.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.comboBoxOutbound.Enabled = false;
             this.comboBoxOutbound.Location = new System.Drawing.Point(81, 58);
             this.comboBoxOutbound.Name = "comboBoxOutbound";
-            this.comboBoxOutbound.Size = new System.Drawing.Size(100, 22);
+            this.comboBoxOutbound.Size = new System.Drawing.Size(100, 21);
             this.comboBoxOutbound.TabIndex = 11;
+            // 
+            // checkBoxComPortSame
+            // 
+            this.checkBoxComPortSame.AutoSize = true;
+            this.checkBoxComPortSame.Checked = true;
+            this.checkBoxComPortSame.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBoxComPortSame.Location = new System.Drawing.Point(15, 82);
+            this.checkBoxComPortSame.Name = "checkBoxComPortSame";
+            this.checkBoxComPortSame.Size = new System.Drawing.Size(178, 17);
+            this.checkBoxComPortSame.TabIndex = 14;
+            this.checkBoxComPortSame.Text = "Use the same COM port for both";
+            this.checkBoxComPortSame.UseVisualStyleBackColor = true;
+            this.checkBoxComPortSame.CheckedChanged += new System.EventHandler(this.checkBoxComPortSame_CheckedChanged);
             // 
             // SettingsForm
             // 
@@ -90,6 +110,7 @@ namespace Haptikos
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.checkBoxComPortSame);
             this.Controls.Add(this.comboBoxOutbound);
             this.Controls.Add(this.comboBoxInbound);
             this.Controls.Add(this.btnCancel);
@@ -100,6 +121,7 @@ namespace Haptikos
             this.Name = "SettingsForm";
             this.Text = "SettingsForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,5 +133,6 @@ namespace Haptikos
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.ComboBox comboBoxInbound;
         private System.Windows.Forms.ComboBox comboBoxOutbound;
+        private System.Windows.Forms.CheckBox checkBoxComPortSame;
     }
 }
